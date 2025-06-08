80036df5809177d54e61c14b5f3712b8ca7b1c82
Milena <ferreyramilenaarlene26@gmail.com>
2025-06-07 21:46:20 -0300

    Ejercicio 1 terminado

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using EspacioCalculadora;
+
+
+Calculadora calcu = new Calculadora();
+int opcion;
+double valor;
+bool seguir = true;
+
+while (seguir) {
+    Console.WriteLine($"\nResultado actual: {calcu.GetResultado()}");
+    Console.WriteLine("Ingrese el numero de la operacion que quiera realizar:");
+    Console.WriteLine("1-Sumar.");
+    Console.WriteLine("2-Restar.");
+    Console.WriteLine("3-Multiplicar.");
+    Console.WriteLine("4-Dividir.");
+    Console.WriteLine("5-Limpiar.");
+    Console.WriteLine("6-Salir.");
+
+    while (!int.TryParse(Console.ReadLine(), out opcion)) {
+        Console.WriteLine("Opción inválida, elija un número entre 1 y 6.");
+    }
+
+    if (opcion == 6)
+    {
+        Console.WriteLine("Saliendo de la calculadora...");
+        seguir = false;
+        break;
+    }
+
+    Console.WriteLine("Ingrese el numero al cual se le realizarà la operaciòn elegida:");
+    while (!double.TryParse(Console.ReadLine(), out valor)) {
+        Console.WriteLine("Numero invàlido, ingrese otro.");
+    }
+
+    switch (opcion)
+    {
+        case 1:
+            calcu.Sumar(valor);
+            break;
+        case 2:
+            calcu.Restar(valor);
+            break;
+        case 3:
+            calcu.Multiplicar(valor);
+            break;
+        case 4:
+            calcu.Dividir(valor);
+            break;
+        case 5:
+            calcu.Limpiar();
+            Console.WriteLine("Calculadora reiniciada.");
+            break;
+    }
+}
+    
